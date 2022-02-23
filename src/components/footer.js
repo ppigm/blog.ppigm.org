a5f59964ae4150884029a2d6fbe6c8eb0f97b706
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-02-23 01:06:39 +0000

    modify footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
     <div className={styles.container}>
       Built with <a href="https://contentful.com/">Contentful</a> and{' '}
       <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      Modified by <a href="https://www.ppigm.org">PPIGM</a>
     </div>
   </Container>
 )
